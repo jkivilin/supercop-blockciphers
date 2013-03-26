4328c28e9029607797b567a0f1358e46b6392f40
Jussi Kivilinna <jussi.kivilinna@iki.fi>
2013-03-26 10:34:59 +0200

    Add twofish128ctr implementations
    Note: contains GPL'd code.
    
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@iki.fi>

---
+++ crypto_stream/twofish128ctr/linux_amd64/twofish.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+/* Structure for an expanded Twofish key.  s contains the key-dependent
+ * S-boxes composed with the MDS matrix; w contains the eight "whitening"
+ * subkeys, K[0] through K[7].	k holds the remaining, "round" subkeys.  Note
+ * that k[i] corresponds to what the Twofish paper calls K[i+8]. */
+struct twofish_ctx {
+	uint32_t s[4][256], w[8], k[32];
+};
+
+void twofish_init(struct twofish_ctx *ctx, const uint8_t *key, uint32_t klen);
+
+void twofish_enc_blk(struct twofish_ctx *ctx, uint8_t *dst, const uint8_t *src);
+void twofish_dec_blk(struct twofish_ctx *ctx, uint8_t *dst, const uint8_t *src);
